64765ae0cf5491a7d16e7eb1d559a8539c5687f4
yvanna Djaha <yvannadjaha@gmail.com>
2025-11-23 13:05:40 +0100

    check

--- Singleton/Singleton.java
+++ Singleton/Singleton.java
@@ -1,50 +1,50 @@
-public final class Singleton{
-    private static Singleton instance=null;
-    
+public final class Singleton {
+    private static Singleton instance = null;
+
     private int x;
     private int y;
 
-    private Singleton(){
+    private Singleton() {
         super();
     }
 
-    //autre singleton
-    private Singleton(int x, int y){
-        this.x=x;
-        this.y=y;
+    private Singleton(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static Singleton getInstance(){
-        if instance==null{
+    public static Singleton getInstance() {
+        if (instance == null) {
             instance = new Singleton();
         }
         return instance;
     }
 
-    public static Singleton getInstance(int x, int y){
-        if instance==null{
-            instance=new Singleton(x, y);
+    public static Singleton getInstance(int x, int y) {
+        if (instance == null) {
+            instance = new Singleton(x, y);
         }
         return instance;
     }
 
-    public inst somme(int x, int y){
-        return x+y;
+    // Méthode somme
+    public int somme(int x, int y) {
+        return x + y;
     }
 
-
-    public float moyenne(int x, int y){
-        return somme(x, y)/2;
-}
-
-    public void affiche(){
-        System.out.println("\nJe suis une instance mes valeurs sont : x = " + this.x + " et y = " +this.y);
+    // Méthode moyenne
+    public float moyenne(int x, int y) {
+        return (float) somme(x, y) / 2;
     }
-//redefinir la méthode clone pour empêcher sonutilisation
+
+    // Affiche les valeurs internes x et y
+    public void affiche() {
+        System.out.println("\nJe suis une instance, mes valeurs sont : x = " 
+            + this.x + " et y = " + this.y);
+    }
 
     @Override
-    public Object clone() throws
-        CloneNotSupportedException {
-            throw new CloneNotSupportedException();
-}
+    public Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
 }
--- Singleton/Test.java
+++ Singleton/Test.java
@@ -1,13 +1,12 @@
-public class Test (){
-    public static void main(args string[]){
-        int som= Singleton.getInstance().somme(2,5);
-        System.out.println("La somme est  %d ",som);
-        Singleton S1=Singleton.getInstance(2,4);
-        S1.affiche()
+public class Test {
+    public static void main(String[] args) {
+        int som = Singleton.getInstance().somme(2, 5);
+        System.out.printf("La somme est %d\n", som);
 
-        Singleton S2 = Singleton.getInstance(5,9);
-        S2.affiche();
+        Singleton s1 = Singleton.getInstance();
+        s1.affiche();
 
-
+        Singleton s2 = Singleton.getInstance();
+        s2.affiche();
     }
 }
